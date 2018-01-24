eeeeef5a54bfa63e41b2b8aaa0c34362a08c7547
Matheus <matheus.moreira@before.com.br>
2018-01-24 11:48:53 -0300

    Criando usuarios com um seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,23 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(CodeFlix\User::class, function (Faker\Generator $faker) {
+
+use CodeFlix\Models\User;
+
+$factory->define(User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->state(User::class, 'admin', function (Faker\Generator $faker) {
+
+    return [
+        'role' => User::ROLE_ADMIN,
     ];
 });
