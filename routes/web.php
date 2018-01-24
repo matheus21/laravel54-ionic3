07f626bb21f11eb63bdcf336d93c5080eb015430
Matheus <matheus.moreira@before.com.br>
2018-01-24 18:06:07 -0300

    Recuperando senha

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,30 @@ Route::get('/', function () {
 });
 
 
-Auth::routes();
+//$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+//$this->post('login', 'Auth\LoginController@login');
+//$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+//
+//// Registration Routes...
+//$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+//$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
+//Auth::routes();
+
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group([
-    'prefix'     => 'admin',
-    'as'         => 'admin.',
-    'namespace'  => 'Admin\\'
+    'prefix'    => 'admin',
+    'as'        => 'admin.',
+    'namespace' => 'Admin\\'
 ], function () {
     Route::name('login')->get('login', 'Auth\LoginController@showLoginForm');
     Route::post('login', 'Auth\LoginController@login');
@@ -32,8 +48,8 @@ Route::group([
         'middleware' => 'can:admin'
     ], function () {
         Route::name('logout')->post('logout', 'Auth\LoginController@logout');
-        Route::get('dashboard', function() {
-            return "Area administrativa functionando";
+        Route::get('dashboard', function () {
+            return view('admin.dashboard');
         });
     });
 });
