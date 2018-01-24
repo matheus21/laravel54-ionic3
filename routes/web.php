77035b59683298bbce6550c04420d94b9d4c6311
Matheus <matheus.moreira@before.com.br>
2018-01-24 13:22:04 -0300

    Autorizando usuarios administrativos

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'can:admin'], function() {
+
+    Route::get('/', function () {
+        return "Area administrativa";
+    });
+
+});
+
+Route::get('/force-login', function() {
+   \Auth::loginUsingId(1);
+});
