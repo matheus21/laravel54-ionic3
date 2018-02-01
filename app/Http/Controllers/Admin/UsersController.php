467fa8c0498408dec8eb2efb056cb9a2fa818ce0
Matheus <matheus.moreira@before.com.br>
2018-02-01 17:07:09 -0300

    Flash Message

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -41,7 +41,7 @@ class UsersController extends Controller
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store()
+    public function store(Request $request)
     {
         $form = \FormBuilder::create(UserForm::class);
 
@@ -52,6 +52,8 @@ class UsersController extends Controller
         $data['role'] = User::ROLE_ADMIN;
         $data['password'] = User::generatePassword();
         User::create($data);
+
+        $request->session()->flash('message', 'Usuário criado com sucesso.');
         return redirect()->route('admin.users.index');
     }
 
